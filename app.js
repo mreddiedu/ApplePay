c66c048b7270cd750a53f293b6f8e43fd85b2103
mreddiedu <mreddiedu@gmail.com>
2021-08-02 14:16:24 -0700

    app listen

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.listen(5000, () => {
+  console.log("App listening on port 5000");
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
